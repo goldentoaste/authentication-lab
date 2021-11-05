6b139d79e40ccf5ac053019c09f11e1c6ac8d4a5
Goldentoaste <rayg2375@gmail.com>
2021-11-05 13:31:07 -0700

    done

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from rest_framework.authtoken.admin import TokenAdmin
+from rest_framework.authtoken.models import Token
+
+TokenAdmin.raw_id_fields = ('user',)
